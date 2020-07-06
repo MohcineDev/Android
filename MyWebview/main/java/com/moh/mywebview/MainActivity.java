6be77a55939f5dd5a3aef98cb553953784268e9a
mouhcinedev <mouhcinedev@gmail.com>
2020-07-07 00:47:27 +0100

    Webview

---
+++ MyWebview/main/java/com/moh/mywebview/MainActivity.java
@@ -0,0 +1,31 @@
+package com.moh.mywebview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.myWeb);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.google.com");
+    }
+}
